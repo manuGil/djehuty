0e0bdbb585cdf2b5d757e0d3a77aa4ee4dc6bde4
Catharina <catharinavaendel@gmail.com>
2024-07-24 16:11:02 +0200

    Web - read groups from xml; and insert groups and supervisor in database & delete group to re-read xml with every djehuty initialize

--- src/djehuty/web/wsgi.py
+++ src/djehuty/web/wsgi.py
@@ -2448,6 +2448,7 @@ class ApiServer:
                                                          account_uuid,
                                                          metadata["read"],
                                                          metadata["edit"],
+                                                         False,
                                                          data["read"],
                                                          data["edit"],
                                                          data["remove"],
@@ -2506,7 +2507,7 @@ class ApiServer:
             if error_response is not None:
                 return error_response
 
-            if self.db.groups[collaborator_uuid]["is_supervisor"]:
+            if value_or(value_or(self.db.groups, collaborator_uuid, None), "is_supervisor", False):
                 return self.error_403 (request)
 
             if self.db.remove_collaborator (dataset["uuid"], collaborator_uuid) is None:
